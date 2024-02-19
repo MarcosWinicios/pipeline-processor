a3852fcd6ff597a15d70daf067084212607a488e
Marcos Winicios Pereira Martins <mwmartins1@topaz.com.br>
2024-02-19 02:31:18 -0300

    fix: Correcting data generation for Excel spreadsheet

--- src/main/java/com/marcos/processor/test/TestConnector.java
+++ src/main/java/com/marcos/processor/test/TestConnector.java
@@ -43,68 +43,7 @@ public class TestConnector {
 		System.out.println("Terminado");
 
 	}
-	
-	
-	public static void connectorListToExcel(ConnectorList connectorList) {
 
-		String[] headers = { "CONNECTOR", "METHOD", "ENDPOINT" };
-
-		int lines = connectorList.getConnectors().size();
-		int columns = headers.length;
-
-		String[][] data = new String[lines][columns];
-
-		System.out.println("linhas: " + lines);
-		System.out.println("columns: " + columns);
-		System.out.println("Tamanho do data: " + data.length);
-
-		for (int i = 0; i < data.length; i++) {
-			var connectorName = "";
-			var httpMethod = "";
-			var endpoint = "";
-
-			connectorName = connectorList.getConnectors().get(i).getName();
-			var size = connectorList.getConnectors().get(i).getRequestList().size();
-
-			if (size > 0) {
-				int auxCount = i;
-				for (int j = 0; j < size; j++) {
-					
-					auxCount = auxCount + j;
-					httpMethod = connectorList.getConnectors().get(i).getRequestList().get(j).getHttpMethod();
-					endpoint = connectorList.getConnectors().get(i).getRequestList().get(j).getEndpoint();
-					
-					data[auxCount][0] = connectorName;
-					data[auxCount][1] = httpMethod;
-					data[auxCount][2] = endpoint;
-				}
-				
-				i = auxCount;
-			}
-
-			System.out.println(connectorName);
-			System.out.println(httpMethod);
-			System.out.println(endpoint);
-
-			data[i][0] = connectorName;
-			data[i][1] = httpMethod;
-			data[i][2] = endpoint;
-			System.out.println("Fim do laço: " + i);
-		}
-
-		printData(data);
-		// Caminho do diretório e nome do arquivo
-		String diretorio = "/home/marcos-winicios/Workspaces/tmp";
-		String nomeArquivo = "newfile.xlsx";
-
-		// Chamada do método para criar e gravar o arquivo Excel
-		try {
-			ExcelUtil.generateExcelFile(headers, data, diretorio, nomeArquivo);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-	}
 
 	public static void printData(String[][] data) {
 		for (int i = 0; i < data.length; i++) {
