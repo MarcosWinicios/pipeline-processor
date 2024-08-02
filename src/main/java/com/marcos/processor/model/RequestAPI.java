44dc4cc19a2135f7aef8a5c24e5e177b005dea11
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2024-08-02 02:52:00 -0300

    feat: Create equals and hashCode methods to RequestAPI class

--- src/main/java/com/marcos/processor/model/RequestAPI.java
+++ src/main/java/com/marcos/processor/model/RequestAPI.java
@@ -3,10 +3,26 @@ package com.marcos.processor.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.Objects;
+
 @Data
 @AllArgsConstructor
 public class RequestAPI {
 	private String httpMethod;
 	private String endpoint;
 	private String description;
+
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		RequestAPI that = (RequestAPI) o;
+		return Objects.equals(httpMethod, that.httpMethod) && Objects.equals(endpoint, that.endpoint);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(httpMethod, endpoint);
+	}
 }
