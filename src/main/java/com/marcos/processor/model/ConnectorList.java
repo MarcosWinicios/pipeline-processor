45b467ea486f96d8eceb1f8d9a8f06d9fa8daa07
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2024-08-02 02:50:16 -0300

    feat: Creating methods to extract endpoints from the ConnectorList object

--- src/main/java/com/marcos/processor/model/ConnectorList.java
+++ src/main/java/com/marcos/processor/model/ConnectorList.java
@@ -1,5 +1,6 @@
 package com.marcos.processor.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import lombok.AllArgsConstructor;
@@ -12,4 +13,21 @@ public class ConnectorList {
 	private String title;
 	
 	private List<Connector> connectors;
+
+	public List<RequestAPI> extractNoDuplicateRequestAPIList(){
+
+		return this.extractRequestAPIList().stream()
+				.distinct()
+				.toList();
+	}
+
+	private List<RequestAPI> extractRequestAPIList(){
+		List<RequestAPI> result = new ArrayList<>();
+
+		connectors.forEach((connector) -> {
+			result.addAll(connector.getRequestList());
+		});
+
+		return result;
+	}
 }
